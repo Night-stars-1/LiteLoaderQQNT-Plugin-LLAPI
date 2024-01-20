a314137bf542a4e380f7ba0e44131a57b31a2fa5
Night-stars-1 <nujj1042633805@gmail.com>
2024-01-20 17:09:31 +0800

    chore: 添加hostname

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -13,7 +13,7 @@ export default defineConfig({
     title: "LLAPI",
     description: "封装部分API，简化开发",
     sitemap: {
-        hostname: ""
+        hostname: "https://llapi.srap.link"
     },
     themeConfig: {
         // https://vitepress.dev/reference/default-theme-config
